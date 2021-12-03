4e983c37dbb69121438aaf7d2433f3ba4c2c4617
Felipe Rocha <61632746+Felps-Oliver@users.noreply.github.com>
2021-12-02 21:45:33 -0300

    Update QUERIES.sql
    Alteração na Query Q2

--- QUERIES.sql
+++ QUERIES.sql
@@ -1,12 +1,12 @@
--->Q1. Obtendo faturamento de 1 mes para trás, com base na data atual
+-->Q1. Obtendo faturamento de 1 mes para trÃ¡s, com base na data atual
 
 SELECT SUM(valor_total_compra) AS Faturamento_Mensal
 FROM COMPRA
 WHERE data_compra BETWEEN (GETDATE() - 30) AND GETDATE()
 
--->Q2. Obtendo total de itens comprados, agrupado por consumidor
+-->Q2. Obtendo total de itens comprados, agrupado por consumidor. Exibindo apenas os TOP 2
 
-SELECT PESSOA.nome AS Consumidor,
+SELECT TOP 2 PESSOA.nome AS Consumidor,
 	PESSOA.CPF AS CPF_Consumidor, 
 	SUM(COMPRA.quantidade_itens) AS Total_Itens
 FROM PESSOA INNER JOIN COMPRA ON PESSOA.CPF = COMPRA.CPF_consumidor
@@ -14,25 +14,25 @@ FROM PESSOA INNER JOIN COMPRA ON PESSOA.CPF = COMPRA.CPF_consumidor
 	ORDER BY PESSOA.nome
 
 
--->Q3. Total de item vendido, preço unitario,
--- quantidade comprada e quantidade disponível,
+-->Q3. Total de item vendido, preÃ§o unitario,
+-- quantidade comprada e quantidade disponÃ­vel,
 -- agrupando pelo nome do item e ordenando-o de forma ascendente
 
 SELECT ITEM.Nome AS Item,
-	ITEM.preço AS Preço_Unitario,
+	ITEM.preÃ§o AS PreÃ§o_Unitario,
 	SUM(ITEM_COMPRADO.quantidade_comprada) AS Quantidade_Comprada,
 	ITEM.quantidade_estoque AS Quantidade_Disponivel
 FROM ITEM LEFT JOIN ITEM_COMPRADO ON ITEM.id = ITEM_COMPRADO.id_item
 	RIGHT JOIN COMPRA ON ITEM_COMPRADO.id_compra = COMPRA.id
-	GROUP BY ITEM.nome, ITEM.preço, ITEM.quantidade_estoque
+	GROUP BY ITEM.nome, ITEM.preÃ§o, ITEM.quantidade_estoque
 	ORDER BY ITEM.nome ASC
 
 
--->Q4.Obter o endereço completo do cadastro de Pessoas residentes
+-->Q4.Obter o endereÃ§o completo do cadastro de Pessoas residentes
 -- em um determinado estado, a quantidade de Rotelland_points
 -- dessa pessoa e calculando sua idade
 
-DECLARE @estado AS varchar(10) = 'São Paulo'
+DECLARE @estado AS varchar(10) = 'SÃ£o Paulo'
 
 SELECT PESSOA.nome AS Nome, 
 	CONCAT(CEP.logradouro, ', ', CEP.bairro, ' - ', CEP.estado, ' - ', CEP.CEP) AS Endereco,
@@ -43,8 +43,8 @@ FROM PESSOA RIGHT JOIN CEP ON PESSOA.CEP = CEP.CEP
 WHERE CEP.estado = @estado
 	ORDER BY NOME
 
--->Q5.Obter a relação de itens de uma determinada categoria, convertendo
--- o tempo de uso de mês para dias, ordenando com base no nome do item
+-->Q5.Obter a relaÃ§Ã£o de itens de uma determinada categoria, convertendo
+-- o tempo de uso de mÃªs para dias, ordenando com base no nome do item
 
 declare @categ as varchar(20) = 'Cadernos'
 
